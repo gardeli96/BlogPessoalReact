08f3a58f8daaa985ee00a68d843112bd00a69d0b
daniel <gardeli96>
2022-05-26 20:37:50 -0300

    atividade react 02

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Grid, Paper } from '@material-ui/core'
+import './Home.css';
+
+function Home() {
+    return (
+        <>
+            <Grid container spacing={2}>
+                <Grid item xs={12} sm ={8}  >
+                    <Paper style={{ height: "100vh", background: "lightgrey" }} />
+                </Grid>
+                <Grid item container direction="column" xs={12} sm={4} spacing={2}>
+                    <Grid item>
+                        <Paper style={{ height: "49vh", background: "orange" }}  />
+                    </Grid>
+                    <Grid item>
+                        <Paper style={{ height: "49vh", background: "green" }}  />
+                    </Grid>
+
+                </Grid>
+            </Grid>
+
+        </>
+
+    );
+}
+export default Home;
